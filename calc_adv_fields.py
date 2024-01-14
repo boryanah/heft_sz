fixed bug with loading only halo pcles through CompaSO and normalization of the delta field; need to make pretty and clean up

calc_adv_fields.py
@@ -62,14 +62,15 @@ def main(path2config, alt_simname=None, verbose=False):
     # all particle files
     pcle_fns = sorted(pcle_dir.glob(f"pcle_info_*.asdf"))
     n_chunks = len(pcle_fns)
-    print(n_chunks)
+    print("number of chunks =", n_chunks)
 
     # get a few parameters for the simulation
     meta = get_meta(sim_name, redshift=0.5)
     Lbox = meta['BoxSize']
     z_ic = meta['InitialRedshift']
     D_growth = meta['GrowthTable'][z_mock]/meta['GrowthTable'][z_ic]
-
+    print("D_growth", D_growth)
+    
     # file to save the advected fields
     adv_fields_fn = Path(save_dir) / f"adv_fields_nmesh{nmesh:d}.asdf"
     
@@ -82,38 +83,41 @@ def main(path2config, alt_simname=None, verbose=False):
     for field in factors_fields.keys():
         adv_fields[field] = np.zeros((nmesh, nmesh, nmesh), dtype=np.float32)
     adv_fields['1cb'] = np.zeros((nmesh, nmesh, nmesh), dtype=np.float32)
-        
+    
     # loop over all files
     for i_chunk, pcle_fn in enumerate(pcle_fns):
         print(i_chunk)
         
         # load particle in this chunk
         data = asdf.open(pcle_fn)['data']
-        lagr_pos = data['pos_lagr']
-        pcle_pos = data['pos_pcle']
+        for part_type in ['halo', 'field']:
+            lagr_pos = np.array(data[f'{part_type}_pos_lagr'][:])
+            pcle_pos = np.array(data[f'{part_type}_pos_pcle'][:])
+            print(lagr_pos[:2], lagr_pos.min(), lagr_pos.max())
+            
+            # get i, j, k for position on the density array
+            lagr_ijk = ((lagr_pos+Lbox/2.)/(Lbox/nmesh)).astype(int)%nmesh # better than without L/2
+            del lagr_pos
 
-        # get i, j, k for position on the density array
-        lagr_ijk = ((lagr_pos+Lbox/2.)/(Lbox/nmesh)).astype(int)%nmesh
-        del lagr_pos
+            # loop over fields
+            for field in factors_fields.keys():
+                # get weight # TODO could speed up by sorting I think
+                w = (f['data'][field]*D_growth**factors_fields[field])[lagr_ijk[:,0], lagr_ijk[:,1], lagr_ijk[:,2]]
 
-        # loop over fields
-        for field in factors_fields.keys():
-            # get weight
-            w = (f['data'][field]*D_growth**factors_fields[field])[lagr_ijk[:,0], lagr_ijk[:,1], lagr_ijk[:,2]]
+                # add to field (it doesn't modify the pcle_pos array)
+                if paste == "TSC":
+                    tsc_parallel(pcle_pos, adv_fields[field], Lbox, weights=w)
+                del w
+                gc.collect()
 
             # add to field
             if paste == "TSC":
-                tsc_parallel(pcle_pos, adv_fields[field], Lbox, weights=w)
-            del w
-            gc.collect()
+                tsc_parallel(pcle_pos, adv_fields['1cb'], Lbox, weights=None)
             
-        # add to field
-        if paste == "TSC":
-            tsc_parallel(pcle_pos, adv_fields['1cb'], Lbox, weights=None)
-
-        # TODO! compensated and interlaced
-        del pcle_pos
-        gc.collect()
+            # TODO! compensated and interlaced
+            del pcle_pos, lagr_ijk
+            gc.collect()
+            print(part_type)
         
     # save fields using asdf compression
     header = {}
@@ -137,7 +141,7 @@ def main(path2config, alt_simname=None, verbose=False):
               #'wa', 'w0',
     ):
         cosmo[k] = meta[k]
-
+                
     # load input linear power
     kth = meta['CLASS_power_spectrum']['k (h/Mpc)']
     pk_z1 = meta['CLASS_power_spectrum']['P (Mpc/h)^3']

save_pcle_disps.py
@@ -14,6 +14,7 @@ from scipy.fft import rfftn, irfftn
 
 from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
 from abacusnbody.metadata import get_meta
+from abacusnbody.data.bitpacked import unpack_pids, unpack_rvint
 
 from obtain_IC_fields import compress_asdf
 
@@ -22,7 +23,7 @@ warnings.filterwarnings('ignore', category=AsdfWarning)
 
 DEFAULTS = {'path2config': 'config/abacus_heft.yaml'}
 
-def read_abacus(chunk_fn):
+def read_abacus_halo(chunk_fn):
     """
     Read positions, velocities and IC positions
     """
@@ -44,6 +45,20 @@ def read_abacus(chunk_fn):
 
     return lagr_pos, pos_mat, vel_mat
 
+def read_abacus(rv_fn, pid_fn, Lbox, PPD):
+
+    # read the halo (L0+L1) matter particles
+    rv_data = asdf.open(rv_fn)['data']['rvint'][:]
+    pid_data = asdf.open(pid_fn)['data']['packedpid'][:]
+
+    # unpack positions
+    pos_mat, vel_mat = unpack_rvint(rv_data, Lbox, float_dtype=np.float32, velout=None)
+    lagr_pos = unpack_pids(pid_data, box=Lbox, ppd=PPD, lagr_pos=True)['lagr_pos']
+    del rv_data, pid_data
+    gc.collect()
+
+    return lagr_pos, pos_mat, vel_mat
+
 def main(path2config, alt_simname=None, verbose=False):
 
     # read heft parameters
@@ -53,6 +68,7 @@ def main(path2config, alt_simname=None, verbose=False):
     kcut = config['heft_params']['kcut']
     z_mock = config['sim_params']['z_mock']
     sim_dir = config['sim_params']['sim_dir']
+    pcle_type = "A"
     
     if alt_simname is not None:
         sim_name = alt_simname
@@ -67,6 +83,8 @@ def main(path2config, alt_simname=None, verbose=False):
     meta = get_meta(sim_name, redshift=z_mock)
     Lbox = meta['BoxSize']
     NP = meta['NP']
+    PPD = int(NP**(1./3))
+    
     # catalog directory
     cat_dir = Path(sim_dir) / sim_name / f"halos" / f"z{z_mock:.3f}" / "halo_info"
     chunk_fns = sorted(cat_dir.glob("halo_info_*.asdf")) #{i_chunk:03d}
@@ -75,7 +93,6 @@ def main(path2config, alt_simname=None, verbose=False):
     
     """
     # estimate number of particles
-    pcle_type = "A"
     if pcle_type == "A":
         N_total = NP*0.03
 
@@ -97,12 +114,21 @@ def main(path2config, alt_simname=None, verbose=False):
     # loop over all chunks
     count = 0
     for i_chunk, chunk_fn in enumerate(chunk_fns):
-        
+        print(i_chunk)
             
-        pos_lagr, pos_pcle, vel_pcle = read_abacus(chunk_fn)
-        N_pcle = pos_lagr.shape[0]
-        print(N_pcle)
+        #pos_lagr, pos_pcle, vel_pcle = read_abacus_halo(chunk_fn) # only halo particles
+        
+        # halo and field particle files
+        # TODO make pretty
+        halo_rv_fn = sim_dir+f'/{sim_name}/halos/z{z_mock:.3f}/halo_rv_{pcle_type}/halo_rv_{pcle_type}_{i_chunk:03d}.asdf'
+        field_rv_fn = sim_dir+f'/{sim_name}/halos/z{z_mock:.3f}/field_rv_{pcle_type}/field_rv_{pcle_type}_{i_chunk:03d}.asdf'
+        halo_pid_fn = sim_dir+f'/{sim_name}/halos/z{z_mock:.3f}/halo_pid_{pcle_type}/halo_pid_{pcle_type}_{i_chunk:03d}.asdf'
+        field_pid_fn = sim_dir+f'/{sim_name}/halos/z{z_mock:.3f}/field_pid_{pcle_type}/field_pid_{pcle_type}_{i_chunk:03d}.asdf'
 
+        # load halo and field particles
+        halo_lagr_pos, halo_pos_mat, halo_vel_mat = read_abacus(halo_rv_fn, halo_pid_fn, Lbox, PPD)
+        field_lagr_pos, field_pos_mat, field_vel_mat = read_abacus(field_rv_fn, field_pid_fn, Lbox, PPD)
+        
         """
         pos_pcles[count: count+N_pcle] = pos_pcle
         vel_pcles[count: count+N_pcle] = vel_pcle
@@ -116,12 +142,17 @@ def main(path2config, alt_simname=None, verbose=False):
         header['Lbox'] = Lbox
         header['pcle_type'] = pcle_type # could be A or B
         table = {}
-        table['pos_pcle'] = pos_pcle
-        table['vel_pcle'] = vel_pcle
-        table['pos_lagr'] = pos_lagr
+        table['halo_pos_pcle'] = halo_pos_mat
+        table['halo_vel_pcle'] = halo_vel_mat
+        table['halo_pos_lagr'] = halo_lagr_pos
+        table['field_pos_pcle'] = field_pos_mat
+        table['field_vel_pcle'] = field_vel_mat
+        table['field_pos_lagr'] = field_lagr_pos
+        pcle_fn = save_dir / f"pcle_info_{i_chunk:03d}.asdf"
         compress_asdf(str(pcle_fn), table, header)
 
-        del pos_lagr, pos_pcle, vel_pcle
+        del halo_lagr_pos, halo_pos_mat, halo_vel_mat
+        del field_lagr_pos, field_pos_mat, field_vel_mat
         gc.collect()
         
         """